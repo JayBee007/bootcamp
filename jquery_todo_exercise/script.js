ad4676ac0cacfd9c7c4cc9fca4620f9108d44294
Jay <jaybee.codes@gmail.com>
2017-01-14 11:05:27 +0300

    basic functionality ready

--- jquery_todo_exercise/script.js
+++ jquery_todo_exercise/script.js
@@ -1,5 +1,19 @@
-$("document").ready(function() {
-  $("li").on("click",function(){
+$("ul").on("click","li",function(){
   	$(this).toggleClass("completed");
   })
-});
+
+  $("ul").on("click","span",function(evt){
+  	$(this).parent().fadeOut(500,function(){
+  	$(this).remove();
+  	});
+  	evt.stopPropagation();
+  });
+
+  $("input").on("keypress",function(evt){
+  	if(evt.which === 13) {
+  		var task = $(this).val();
+  		$(this).val("");
+  		$("ul").append("<li><span><i class='fa fa-trash'></i></span>"+" "+task+"</li>");
+
+  	}
+  });
